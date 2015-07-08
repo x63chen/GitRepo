a748ede217419cff16b2187540013b035ab655d3
x63chen <chen2jason@hotmail.com>
2015-07-08 10:33:48 -0400

    Updated MongoDB with new connection

--- NodeJS/config/db.js
+++ NodeJS/config/db.js
@@ -1,4 +1,4 @@
 // config/db.js
-    module.exports = {
-        url : 'mongodb://localhost/stencil-dev'
-    }
+module.exports = {
+    url : 'mongodb://rbcUser:Password1@ds047802.mongolab.com:47802/mongobluemixcharity'
+}
