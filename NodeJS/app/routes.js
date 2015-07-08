b8f483cbe5ed043f616fa140d33df6e2b2d29170
divakaran <divakaran.sundaramoorthy@gmail.com>
2015-07-08 12:04:52 -0400

    added user reg. model and routes

--- NodeJS/app/routes.js
+++ NodeJS/app/routes.js
@@ -2,6 +2,7 @@
 
 // grab the nerd model we just created
 var Nerd = require('./models/nerd');
+var User = require('./models/User');
 
     module.exports = function(app) {
 
@@ -14,7 +15,7 @@ var Nerd = require('./models/nerd');
             // use mongoose to get all nerds in the database
             Nerd.find(function(err, nerds) {
 
-                // if there is an error retrieving, send the error. 
+                // if there is an error retrieving, send the error.
                                 // nothing after res.send(err) will execute
                 if (err)
                     res.send(err);
@@ -22,6 +23,38 @@ var Nerd = require('./models/nerd');
                 res.json(nerds); // return all nerds in JSON format
             });
         });
+
+        app.post('/api/register', function(req, res) {
+          // Get our form values. These rely on the "name" attributes
+          var newUser = new User();
+
+          newUser.userid = req.body.userid;
+          newUser.firstname = req.body.firstname;
+          newUser.lastname = req.body.lastname;
+          newUser.email = req.body.email;
+          newUser.password = req.body.password;
+          newUser.addressL1 = req.body.addressL1;
+          newUser.addressL2 = req.body.addressL2;
+          newUser.phone = req.body.phone;
+          newUser.comments = req.body.comments;
+          // Save the data
+          newUser.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'User created!' });
+          });
+
+          res.send("There was a problem adding the information to the database.");
+      }
+      else {
+          res.send("Registration was completed successfully.");
+          // Or forward to success page
+          //res.redirect("userlist"); // to the userlist page... if necessory!
+      }
+
+      });
+    });
 
         // route to handle creating goes here (app.post)
         // route to handle delete goes here (app.delete)
